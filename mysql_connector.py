f28bfb1c404c1ec093034c1b965de19b9565cea9
Nathan <nathanp22@vt.edu>
2025-10-24 00:43:45 -0400

    hosted database and fixed connection

--- mysql_connector.py
+++ mysql_connector.py
@@ -4,11 +4,12 @@ load_dotenv()
 import os
 
 mydb = mysql.connector.connect(
-host='localhost',
-user='root',
-password=os.getenv('DB_PASSWORD'),
-port='3306',
-database='Project'
+    host=os.getenv('DB_HOST'),          
+    user=os.getenv('DB_USER'),          
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT'),          
+    database=os.getenv('DB_DATABASE'),
+    ssl_ca='ca.pem' 
 )
 
 mycursor = mydb.cursor()
@@ -19,3 +20,6 @@ mycursor.execute("select * from Employee")
 data = mycursor.fetchall()
 for i in data:
     print(f"First_Name: {i[0]}, Last_Name: {i[1]}, Employee_ID: {i[2]}, Phone_Number: {i[3]}")
+
+mycursor.close()
+mydb.close()
