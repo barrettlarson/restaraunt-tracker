0266c8b7f146f5d97c9c30a31ec59be750b716e5
Nathan <nathanp22@vt.edu>
2025-10-23 22:35:10 -0400

    test

--- mysql_connector.py
+++ mysql_connector.py
@@ -15,7 +15,7 @@ mycursor = mydb.cursor()
 #mycursor.execute('insert into Employee(First_name,Last_Name, Employee_ID) values("Nate2", "Pham2", 321)')
 #mydb.commit()
 mycursor.execute("select * from Employee")
-##
+
 data = mycursor.fetchall()
 for i in data:
     print(f"First_Name: {i[0]}, Last_Name: {i[1]}, Employee_ID: {i[2]}, Phone_Number: {i[3]}")
